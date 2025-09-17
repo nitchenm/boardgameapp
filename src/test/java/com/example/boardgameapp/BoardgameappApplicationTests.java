b0af5e0331dd88f98e451e6e18e68cb2c3941c92
nitchenm <nitchenm@gmail.com>
2025-09-17 19:28:31 -0300

    first commit

---
+++ src/test/java/com/example/boardgameapp/BoardgameappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardgameapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgameappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
